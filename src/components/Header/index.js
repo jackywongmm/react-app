49980c306d1dc7201cc8f0fcbc718dc48803f527
Jacky <771400065@qq.com>
2019-07-02 18:12:47 +0800

    add scss loader

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import './index.scss'
 
 class Header extends Component {
   render () {
     return (
-      <div>
+      <header>
         <ul>
           <li>首页</li>
           <li>个人中心</li>
+          <li>退出</li>
         </ul>
-      </div>
+      </header>
     )
   }
 }
